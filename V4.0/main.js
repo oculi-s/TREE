dd399e587434dc8613d2b17d6659968ec2590e9e
oculi-s <kjunhun9@sogang.ac.kr>
2021-12-10 13:50:32 +0900

    211210

---
+++ V4.0/main.js
@@ -0,0 +1,28 @@
+const $ = document.querySelector.bind(document);
+
+var _fi = $('#_fi');
+var _cn = $('#_cn');
+var _mu = $('#_mu');
+var _su = $('#_su');
+_su.value = String.fromCharCode(9654);
+
+function _file_check() {
+    if (_fi.files.length == 0) {
+        alert('No file uploaded')
+    }
+    else {
+        _file = _fi.files[0];
+        _csv_to_table(_file);
+    }
+}
+
+const MAX_DEPTH = 5;
+const t = $('article table');
+for (var r = 0; r < 20; r++) {
+    var temp = `<tr>`;
+    for (var c = 0; c<MAX_DEPTH; c++){
+        temp += `<td data-c=${c} data-r=${r}><input type=text></input></td>`
+    }
+    temp += `</tr>`;
+}
+t.innerHTML += temp;
