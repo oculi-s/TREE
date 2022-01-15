994edb6d0d86e12fa61f348222addee79a06ba9c
oculi-s <oculis0925@yonsei.ac.kr>
2022-01-15 20:18:39 +0900

    220115

--- V4.1/main.js
+++ V4.1/main.js
@@ -25,55 +25,47 @@ function wresize() {
     }
 }
 
-var isend = 0;
-
-/* function index(a, b){
-        var i;
-    for (i=0; i<a.length; i++){
-            if (b==a[i])
-            return i;
-    }
-    return -1;
-} */
+var ise = 0;
+var isf = 0;
 
 function cmove(e) {
     var k = event.keyCode;
     if (k > 36 && k < 41) {
-        var sel = window.getSelection();
+        var o = getSelection().anchorOffset;
         var r = t.childNodes.index(e.parentNode);
         var c = t.childNodes[r].childNodes.index(e);
-        e.innerText = e.innerText.replace('\n', '');
+        var n = e.innerText.length;
         if (k == 38 && r > 0) {
             t.children[r - 1].children[c].focus();
         } else if (k == 40) {
             if (r == MAX_DATA - 2) { add_row(); }
             t.children[r + 1].children[c].focus();
         } else if (k == 37 && c > 0) {
-            if (!e.innerText) {
-                e.previousSibling.focus();
-            } else if (sel.anchorOffset == 1) {
-                isend = 1;
-            } else if (!sel.anchorOffset) {
-                if (!isend) {
-                    isend = 1;
+            if (!n) {
+                e.previousElementSibling.focus();
+            } else if (o == 1) {
+                isf = 1;
+            } else if (o == 0) {
+                if (isf) {
+                    e.previousElementSibling.focus();
+                    isf = 0;
                 } else {
-                    e.previousSibling.focus();
-                    isend = 0;
+                    isf = 1;
                 }
             }
         } else if (k == 39) {
-            if (!e.innerText) {
+            if (!n) {
                 if (c == MAX_DEPTH - 2) { add_col(); }
-                t.children[r].children[c + 1].focus();
-            } else if (sel.anchorOffset == e.innerText.length - 1) {
-                isend = 1;
-            } else if (sel.anchorOffset == e.innerText.length) {
-                if (!isend) {
-                    isend = 1;
-                } else {
+                e.nextElementSibling.focus();
+            } else if (o == n - 1) {
+                ise = 1;
+            } else if (o == n) {
+                if (ise) {
                     if (c == MAX_DEPTH - 2) { add_col(); }
-                    t.children[r].children[c + 1].focus();
-                    isend = 0;
+                    e.nextElementSibling.focus();
+                    ise = 0;
+                } else {
+                    ise = 1;
                 }
             }
         }
@@ -84,14 +76,14 @@ function cmove(e) {
 }
 
 function td(r, c) {
-    return $$('tr')[r].children[c].innerText.replace('\n', '');
+    return t.children[r].children[c].innerText.replace('\n', '');
 }
 
 function table_to_dict() {
     for (var i = 0; i < MAX_DATA; i++) {
         if (td(i, 0)) {
             var j = i + 1;
-            while (!td(j, 0) && j++ < MAX_DATA - 1) { }
+            while (!td(j, 0) && j++ < MAX_DATA - 1) {}
             add_data(dict, td(i, 0), i, j, 1);
         }
     }
@@ -102,7 +94,7 @@ function add_data(sub, c, s, e, DEPTH) {
     for (var i = s + 1; i < e; i++) {
         if (td(i, DEPTH) && DEPTH < MAX_DEPTH - 1) {
             var j = i + 1;
-            while (!td(j, DEPTH) && j++ < MAX_DATA - 1) { }
+            while (!td(j, DEPTH) && j++ < MAX_DATA - 1) {}
             if (td(i + 1, DEPTH + 1)) {
                 add_data(sub[c], td(i, DEPTH), i, j, DEPTH + 1);
             } else {
@@ -133,6 +125,7 @@ function add_string(sub, b, DEPTH) {
 }
 
 function convert() {
+    $$('td').forEach(e => { e.innerText = e.innerText.replaceAll('\n', ''); })
     string = '';
     dict = {};
     table_to_dict();
@@ -167,7 +160,7 @@ function rem_row() {
 function upload() {
     var inp = $('input[type=file]');
     inp.click();
-    inp.onchange = async () => {
+    inp.onchange = async() => {
         $$('td').forEach(e => { e.innerHTML = ''; });
         var csv = await inp.files[0].text();
         csv = csv.replaceAll('\n', '').split('\r');
@@ -179,7 +172,6 @@ function upload() {
             var row = csv[i].split(',');
             for (j = 0; j < row.length; j++) {
                 t.children[i].children[j].innerText = row[j].replace(/\n|\r*/g, "");
-                t.children[i].children[j].innerText.replace('\n', '');
             }
         }
         while (!t.children[MAX_DATA - 2].innerText) { rem_row(); }
